363d490142fb55a3aca511ca792104ee747e22ba
zannazw <zanna.wahedi@outlook.com>
2026-06-14 00:38:25 +0200

    initial commit

---
+++ src/main/java/de/hawhamburg/inf/ea/zug/Zug.java
@@ -0,0 +1,133 @@
+package de.hawhamburg.inf.ea.zug;
+
+import io.jenetics.prog.op.Op;
+
+/**
+ * @author chris
+ */
+public class Zug {
+
+    public static abstract class Operation implements Op<Double> { // Knoten im Baum
+
+        protected Zug zug;
+
+        public void setZug(Zug zug) {
+            this.zug = zug;
+        }
+
+        @Override
+        public int arity() {
+            return 1;
+        }
+
+        @Override
+        public String toString() {
+            return name();
+        }
+    }
+
+    public static class GetSpeed extends Operation { // Ist eine Operation
+
+        @Override
+        public String name() {
+            return "GET_SPEED";
+        }
+
+        @Override
+        public int arity() {
+            return 0;
+        } // arity sagt Länge des Array
+
+        @Override // bekommt ein Array, Länge Array = Argumente bzw. Anzahl Kinder
+        public Double apply(Double[] t) {
+            return zug.getGeschwindigkeit();
+        } // Keine weiteren Argumente = Terminal
+
+    }
+
+    public static class GetDistance extends Operation {
+
+        @Override
+        public String name() {
+            return "GET_DIST";
+        }
+
+        @Override
+        public int arity() {
+            return 0;
+        }
+
+        @Override
+        public Double apply(Double[] t) {
+            return zug.getEntfernung();
+        }
+
+    }
+
+    public static class IfElse extends Operation {
+
+        @Override
+        public String name() {
+            return "IF";
+        }
+
+        @Override
+        public int arity() {
+            return 3;
+        }
+
+        @Override
+        public Double apply(Double[] t) { // 3 Argumente = Funktion
+            if (t[0] > 0) { // true
+                return t[1];
+            } else {
+                return t[2];
+            }
+        }
+
+    }
+
+    public static class SetSpeed extends Operation {
+
+        @Override
+        public String name() {
+            return "SET_SPEED";
+        }
+
+        @Override
+        public Double apply(Double[] v) { // 1 Argument = Funktion
+            zug.setGeschwindigkeit(v[0]);
+            return v[0];
+        }
+
+    }
+
+    private double entfernung;
+    private double geschwindigkeit;
+    private double energie;
+
+    public void setGeschwindigkeit(double geschwindigkeit) {
+        this.geschwindigkeit = geschwindigkeit;
+    }
+
+    public double getEnergie() {
+        return this.energie;
+    }
+
+    public double getGeschwindigkeit() {
+        return geschwindigkeit;
+    }
+
+    public double getEntfernung() {
+        return entfernung;
+    }
+
+    /**
+     * Ein Zeitschritt.
+     */
+    public void tick() {
+        // Physiker*innen bitte wegsehen...
+        this.entfernung += geschwindigkeit;
+        this.energie += geschwindigkeit * geschwindigkeit;
+    }
+}
---
+++ src/main/java/de/hawhamburg/inf/ea/zug/Zugfahrt.java
@@ -0,0 +1,94 @@
+package de.hawhamburg.inf.ea.zug;
+
+import io.jenetics.Genotype;
+import io.jenetics.Mutator;
+import io.jenetics.engine.Engine;
+import io.jenetics.engine.EvolutionResult;
+import io.jenetics.engine.Limits;
+import io.jenetics.ext.SingleNodeCrossover;
+import io.jenetics.ext.util.TreeNode;
+import io.jenetics.prog.ProgramChromosome;
+import io.jenetics.prog.ProgramGene;
+import io.jenetics.prog.op.Const;
+import io.jenetics.prog.op.MathOp;
+import io.jenetics.prog.op.Op;
+import io.jenetics.util.ISeq;
+
+/**
+ * @author chris
+ */
+public class Zugfahrt {
+
+    public static double error(Genotype<ProgramGene<Double>> ind) {
+        Zug zug = new Zug();
+
+        // Setze die Zug instance bei allen Operationen und Terminalen
+        for (var op : ind.gene().operations()) {
+            if (op instanceof Zug.Operation operation) {
+                operation.setZug(zug);
+            }
+        }
+
+        for (var op : ind.gene().terminals()) {
+            if (op instanceof Zug.Operation operation) {
+                operation.setZug(zug);
+            }
+        }
+
+        var gene = ind.gene(); // hol den GP-Baum aus diesem Individuum heraus
+        for (int i = 0; i < 100; i++) { // Maximal 100 Schritte
+            // Wir rufen das Kontrollprogramm des Zugs auf.
+            // Je nach Entfernung (und ggfls. aktueller Geschwindigkeit) soll
+            // das Programm die neue Geschwindigkeit des Zuges setzen.
+            // Der Kontroll-Loop wird durch die For-Schleife hier repräsentiert,
+            // weil dies in GP schwierig als Operation darzustellen ist.
+            gene.eval(); // frage den Regler EINMAL: setzt neue Geschwindigkeit und werte diesen Baum rekursiv aus
+
+            // Wir lassen Entfernung und Energie aktualisieren
+            zug.tick(); // ein Zeitschritt: Zug rollt, Energie steigt
+            if (zug.getEntfernung() - 1000.0 >= 0) {
+                break;
+            }
+        }
+
+        return Math.abs(zug.getEntfernung() - 1000) + zug.getEnergie();
+    }
+
+    public static void main(String[] args) {
+        final ISeq<Op<Double>> operations = ISeq.of( // unveränderliche Liste
+                new Zug.SetSpeed(),
+                new Zug.IfElse(),
+                MathOp.ADD, MathOp.SUB);
+
+        final ISeq<Op<Double>> terminals = ISeq.of(
+                new Zug.GetSpeed(),
+                new Zug.GetDistance(),
+                Const.of(0.0), Const.of(1.0), Const.of(2.0), Const.of(3.0),
+                Const.of(-1.0), Const.of(-2.0), Const.of(-3.0)
+        );
+
+        final ProgramChromosome<Double> program // Bauplan für Bäume
+                = ProgramChromosome.of(10, operations, terminals); // Baumtiefe: 10
+
+        final Engine<ProgramGene<Double>, Double> engine = Engine // ProgramGene = EIN GP-Baum
+                .builder(Zugfahrt::error, program) // Fitnessfunktion + Baum-Bauplan
+                .minimizing() // kleiner Fitnesswert = besser
+                .alterers( // welche genetischen Operatoren?
+                        new SingleNodeCrossover<>(), // Subtree-Crossover
+                        new Mutator<>()) // Subtree-Mutation
+                .build();
+
+        final EvolutionResult<ProgramGene<Double>, Double> result = engine // Evolution laufen lassen
+                .stream()
+                .limit(Limits.byFixedGeneration(10))
+                .collect(EvolutionResult.toBestEvolutionResult());
+
+
+        final TreeNode<Op<Double>> tree = result.bestPhenotype()
+                .genotype().gene().toTreeNode();
+
+        System.out.println("Generations: " + result.totalGenerations());
+        System.out.println("Function:    " + tree);
+        System.out.println("Error:       " + error(result.bestPhenotype().genotype()));
+    }
+}
